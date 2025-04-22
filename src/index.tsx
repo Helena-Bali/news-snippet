b72a4c6de6d35ad8c5896a4422a282082a22abba
Helena-Bali <elenabaltajs@gmail.com>
2025-04-22 16:44:29 +0300

    creating interface file

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NewsApp from "./components/NewsApp";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <NewsApp />
   </React.StrictMode>
 );
 
